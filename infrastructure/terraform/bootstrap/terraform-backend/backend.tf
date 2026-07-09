34369e2d541c4e010328ce15a18ce1bb3db30ea8
Gabin <gabinsime@yahoo.com>
2026-07-08 21:29:23 -0500

    feat: updated cloudhustler-tfstate-dev

--- infrastructure/terraform/bootstrap/terraform-backend/backend.tf
+++ infrastructure/terraform/bootstrap/terraform-backend/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "cloudhustler-tfstate-prod"
+    bucket       = "cloudhustler-tfstate-dev"
     key          = "bootstrap/backend/terraform.tfstate"
     region       = "us-east-2"
     use_lockfile = true
